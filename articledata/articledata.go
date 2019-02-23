0fc3678f9bce473b7ec01455e19fe12f1b0dae90
neutronest <neutronest@gmail.com>
2019-02-24 01:49:16 +0800

    [@neutronest] parse concrete article

--- articledata/articledata.go
+++ articledata/articledata.go
@@ -7,9 +7,47 @@ import (
 	"strings"
 	"encoding/json"
 	"strconv"
+	"github.com/anaskhan96/soup"
 	"github.com/neutronest/anitamasaver/anitama"
 	"github.com/bradfitz/slice"
 )
+
+func parseArticleContent(articleURL string, articleMetadata anitama.ArticleMetaData) {
+	fmt.Println(articleURL)
+	response, err := soup.Get(articleURL)
+	if err != nil {
+		fmt.Println("response error")
+		os.Exit(1)
+    }
+	doc := soup.HTMLParse(response)
+	mainDiv := doc.Find("div", "id", "mainer")
+	innerDiv := mainDiv.Find("div", "class", "inner")
+	// topArticleDiv := innerDiv.Find("div", "id", "area-top-article")
+	// style := topArticleDiv.Attrs()["style"]
+	// fmt.Println(topArticleDiv.Text())
+	// fmt.Println(style)
+	if innerDiv.Error != nil {
+		fmt.Println("innerDiv error")
+	}
+	fmt.Println(innerDiv.Pointer.Data, innerDiv.NodeValue)
+	contentDiv := innerDiv.Find("div", "id", "area-content-article")
+	data := contentDiv.Children()
+	for _, component := range data {
+
+		if (component.NodeValue == "p") {
+			if component.Children()[0].NodeValue == "img" {
+				fmt.Println(component.Children()[0].Attrs()["data-src"])
+			} else {
+				fmt.Println(component.Text() + "\n")
+			}
+		} else if (component.NodeValue == "blockquote") {
+			fmt.Println("[==BLOCKQUOTE WARNIGN===]")
+			fmt.Println(component.FullText())
+		} 
+		
+	}
+
+}
 
 func main() {
 	fmt.Println("Hello")
@@ -44,6 +82,8 @@ func main() {
 	for _, articleMetadata := range articleMetadatas {
 		fmt.Println(articleMetadata.Title, articleMetadata.Id)
 	}
+
+	parseArticleContent(articleMetadatas[0].Url, articleMetadatas[0])
 
 
 }
